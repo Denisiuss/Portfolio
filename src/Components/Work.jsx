9a24ab7553c94f7b26112d707f51f67bcb8b3f0b
Denisiuss <vasilevski12@mail.ru>
2023-09-09 19:25:04 +0300

    adding netflix to prtflio

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -5,6 +5,7 @@ import GoogleTranslate from '../Assets/GoogleTranslate.png'
 import Website from '../Assets/WebsitePortfolio.png'
 import ToDoApp from '../Assets/ToDoApp.png'
 import Wordle from '../Assets/Wordle.png'
+import Netflix from '../Assets/Netflix.png'
 
 const Work = () => {
   return (
@@ -14,8 +15,25 @@ const Work = () => {
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
                 <p className='py-6'>// Check out some of my recent work</p>
             </div>
-
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            <div style={{backgroundImage: `url(${Netflix})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Netflix Clone
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://github.com/Denisiuss/react-netflix-clone">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                            <a href="https://netflix-react-app-pt.web.app/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
                 <div style={{backgroundImage: `url(${MTracker})`}} 
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     <div className='opacity-0 group-hover:opacity-100'>
@@ -101,22 +119,7 @@ const Work = () => {
                     </div>
                 </div>
 
-                <div style={{backgroundImage: `url(${Wordle})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            Wordle Game Clone
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="https://github.com/Denisiuss/wordleClone.github.io">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                            <a href="https://denisiuss.github.io/wordleClone.github.io/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
